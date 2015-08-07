53d0553dcd83d458f81d0c5cbf45f86abf936a28
Ariel Ibarra <arjos8@gmail.com>
2015-08-06 19:39:43 -0600

    - changed color schema

--- wp-content/themes/eddiemachado-bones-9db85e4/index.php
+++ wp-content/themes/eddiemachado-bones-9db85e4/index.php
@@ -32,28 +32,67 @@
                                 <img src="<?php echo get_template_directory_uri(); ?>/library/images/icon_07.png">
                                 Job Experience
                             </h2>
-                            <p class="job-title"><span>Web Developer:</span> BVMedia. FEB 2014 - PRESENT</p>
-                            <p>
-                                As a Web Developer at BVMedia, I contributed to the development, execution, quality assurance and testing of all marketed web sites.
-                                I built and maintained existing PHP code, MySQL databases. Developed custom PHP solutions using Laravel Framework. Developed and maintained custom WordPress themes and also helped in creation and improvement of JAVA-based websites using Hibernate CRM.
-                            </p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1">
-                            <p class="job-title"><span>Web Developer:</span> BVMedia. FEB 2014 - PRESENT</p>
+                            <p class="job-title"><span>Web Developer:</span> BVMedia. FEB 2014 - JULY 2015</p>
                             <p>
-                                As a Web Developer at BVMedia, I contributed to the development, execution, quality assurance and testing of all marketed web sites.
-                                I built and maintained existing PHP code, MySQL databases. Developed custom PHP solutions using Laravel Framework. Developed and maintained custom WordPress themes and also helped in creation and improvement of JAVA-based websites using Hibernate CRM.
+                                As a Web Developer at BVMedia, I contributed to the development, execution, quality
+                                assurance and testing of all marketed web sites.
+                            </p><p>
+                                I built and maintained existing PHP code, MySQL databases. Developed custom PHP
+                                solutions using Laravel Framework. Developed and maintained custom WordPress themes and
+                                also helped in creation and improvement of JAVA-based websites using Hibernate CRM.
                             </p>
                         </div>
                     </div>
+                    <div class="separator"></div>
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1">
-                            <p class="job-title"><span>Web Developer:</span> BVMedia. FEB 2014 - PRESENT</p>
+                            <p class="job-title"><span>Creative Engineer:</span> Prodigious LatAm. APR 2013 - FEB 2014</p>
                             <p>
-                                As a Web Developer at BVMedia, I contributed to the development, execution, quality assurance and testing of all marketed web sites.
-                                I built and maintained existing PHP code, MySQL databases. Developed custom PHP solutions using Laravel Framework. Developed and maintained custom WordPress themes and also helped in creation and improvement of JAVA-based websites using Hibernate CRM.
+                                On my time working as C.E at Prodigious ( Previously BoszDigital ) I built and
+                                maintained websites and Landing Pages for brands‘ like Whirlpool, Maytag and KitchenAid.
+                                Also contributed in the backend (C#) maintenance of those brand‘s websites.
+                            </p><p>
+                                I was in charge of the weekly developer meetings, setting up the agenda and bringing up
+                                new ideas, technologies or tips for the daily job.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <p class="job-title"><span>Associate Creative Engineer:</span> BoszDigital. FEB 2012 - APR 2013</p>
+                            <p>
+                                As an A.C.E at Bosz, my main duties were to maintain websites and develop landing pages
+                                using technologies like jQuery, Javascript and CSS.
+                            </p><p>
+                                With the help of my team-mate and our dedication and hard work, we both demonstrated to
+                                our current client that we were capable of more and challenging work. We helped the team
+                                grow from 2 developers to almost 10 developers.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <p class="job-title"><span>Consultant I, Advisory IT:</span> PriceWaterhouseCoopers. JULY 2011 - FEB 2012</p>
+                            <p>
+                                Working for PricewaterhouseCoopers was the perfect beginning. I developed their HHRR
+                                internal website that stores their Resumes Database. This system was built with
+                                Microsoft technologies using C#, ASP, SQL Server, HTML, CSS and a little bit of JS.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <p class="job-title"><span>JR Web Developer:</span> SealSystems, Corp. MAY 2009 - NOV 2009</p>
+                            <p>
+                                A very little startup located in David, Chiriqui, Republic of Panama. I assisted in the
+                                developemt of websites built in PHP and Prototype.JS.
                             </p>
                         </div>
                     </div>
@@ -63,7 +102,7 @@
                                 <img src="<?php echo get_template_directory_uri(); ?>/library/images/icon_11.png">
                                 Awards
                             </h2>
-                            <p class="job-title"><span>Web Developer:</span> BVMedia. FEB 2014 - PRESENT</p>
+                            <p class="job-title"><span>Top Performance:</span> at Boszdigital as Associate C.E</p>
                         </div>
                     </div>
                 </div>
@@ -77,6 +116,9 @@
                             <h2 class="my-name">Ariel Ibarra</h2>
                             <h4 class="position">Full-Stack Web Developer</h4>
                             <img class="passport-photo" src="<?php echo get_template_directory_uri(); ?>/library/images/passport-photo.png">
+                            <p class="job-title" style="text-align: center">
+                                <span>CURRENT POSITION</span><br>Web Developer<br>at Rivet Logic Corporation
+                            </p>
                         </div>
                     </div>
                     <div class="row">
