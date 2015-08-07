b9469d1024aa89f71dd33efcf1fa87d9af740038
Ariel Ibarra <arjos8@gmail.com>
2015-08-06 23:11:22 -0600

    - sprint-1 ready

--- wp-content/themes/eddiemachado-bones-9db85e4/index.php
+++ wp-content/themes/eddiemachado-bones-9db85e4/index.php
@@ -1,8 +1,79 @@
 <?php get_header(); ?>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-8 left-col">
+        <div class="col-md-3 right-col">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="my-name">Ariel Ibarra</h2>
+                            <h4 class="position">Full-Stack Web Developer</h4>
+                            <img class="passport-photo" src="<?php echo get_template_directory_uri(); ?>/library/images/passport-photo.png">
+                            <p class="job-title" style="text-align: center">
+                                <span>CURRENT POSITION</span><br>Web Developer<br>at Rivet Logic Corporation
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 info-box">
+                            <ul>
+                                <li class="address">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_03.png">
+                                    San José, Costa Rica</li>
+                                <li class="phone">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_06.png">
+                                    +506 6196-6138
+                                </li>
+                                <li class="email">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_08.png">
+                                    arjos8@gmail.com
+                                </li>
+                                <li class="linkein">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_10.png">
+                                    <a href="https://cr.linkedin.com/in/aibarrab">LinkedIn Profile</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="skills light-blue">Skills</h2>
+                            <p class="job-title"><span>Professional skills</span></p>
+                            <p class="skills-list">
+                                JS + jQuery + <b>RequireJS</b> + OOP JS +
+
+                                CSS + CSS3 + Bootstrap + HTML5 +
+
+                                WordPress + <b>Laravel</b> + HTML +
+
+                                <b>Responsive</b> Web Development +
+
+                                PHP + C# + JAVA + MySQL + <b>GIT</b> +
+
+                                <b>Git Branching Model</b> + SVN +
+
+                                <b>Javascript Design Patterns</b> + MVC
+                            </p>
+                            <hr>
+                        </div>
+                        <div class="col-md-12">
+                            <p>WHAT THEY SAY:</p>
+                            <p>
+                                Ana Victoria Chacon<br>
+                                Senior SQA Analyst at Prodigious
+                            </p>
+                            <p>
+                                "Ariel is always looking for challenging projects that let him improve his skills and
+                                knowledge, he enjoys learning new technologies and he is always ready to help others on
+                                the daily work."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 left-col">
             <div class="row">
                 <div id="intro" class="col-md-10 col-md-offset-1">
                     <h1 class="light-blue">Hello !</h1>
@@ -105,75 +176,12 @@
                             <p class="job-title"><span>Top Performance:</span> at Boszdigital as Associate C.E</p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 right-col">
-            <div class="row">
-                <div class="col-md-9 col-md-offset-1">
                     <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="my-name">Ariel Ibarra</h2>
-                            <h4 class="position">Full-Stack Web Developer</h4>
-                            <img class="passport-photo" src="<?php echo get_template_directory_uri(); ?>/library/images/passport-photo.png">
-                            <p class="job-title" style="text-align: center">
-                                <span>CURRENT POSITION</span><br>Web Developer<br>at Rivet Logic Corporation
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 info-box">
-                            <ul>
-                                <li class="address">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_03.png">
-                                    Montes de Oca, Costa Rica</li>
-                                <li class="phone">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_06.png">
-                                    +506 6196-6138
-                                </li>
-                                <li class="email">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_08.png">
-                                    arjos8@gmai.com
-                                </li>
-                                <li class="linkein">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/icons_10.png">
-                                    <a href="cr.linkedin.com/in/aibarrab">cr.linkedin.com/in/aibarrab</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="skills light-blue">Skills</h2>
-	                        <p class="job-title"><span>Professional skills</span></p>
-                            <p class="skills-list">
-                                JS + jQuery + <b>RequireJS</b> + OOP JS +
-
-                                CSS + CSS3 + Bootstrap + HTML5 +
-
-                                WordPress + <b>Laravel</b> + HTML +
-
-                                <b>Responsive</b> Web Development +
-
-                                PHP + C# + JAVA + MySQL + <b>GIT</b> +
-
-                                <b>Git Branching Model</b> + SVN +
-
-                                <b>Javascript Design Patterns</b> + MVC
-                            </p>
-	                        <hr>
-                        </div>
-                        <div class="col-md-12">
-                            <p>WHAT THEY SAY:</p>
-                            <p>
-                                Ana Victoria Chacon<br>
-                                Senior SQA Analyst at Prodigious
-                            </p>
-                            <p>
-                                "Ariel is always looking for challenging projects that let him improve his skills and
-                                knowledge, he enjoys learning new technologies and he is always ready to help others on
-                                the daily work."
-                            </p>
+                        <div class="col-md-10 col-md-offset-1">
+                            <a href="/downloads/ariel_ibarra_resume.pdf" target="_blank" class="btn btn-default download">
+                                <span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+                                Get PDF Version
+                            </a>
                         </div>
                     </div>
                 </div>
